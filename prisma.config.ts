d75d471af6129d1aa511ccede146663c916eddbc
BSM-Code-777 <bruno.sm.code@hotmail.com>
2026-06-19 19:45:07 -0300

    alteração prisma config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  engine: "classic", // Diz ao Prisma para conectar direto ao Supabase tradicional
   datasource: {
     url: env("DATABASE_URL"),
   },
